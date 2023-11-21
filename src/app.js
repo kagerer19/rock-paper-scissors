33e0c13d656a08a347c7489ba3903d95d134d5e5
kagerer19 <kagerer19@gmail.com>
2023-11-21 12:50:33 +0100

    Complete, rock paper, scissors

--- src/app.js
+++ src/app.js
@@ -1,89 +1,106 @@
-// Cache DOM
 let userScore = 0;
-let AIScore = 0;
-let userScore_span = document.getElementById("user-score");
-let aiScore_span = document.getElementById("ai-score");
-const scoreBoard = document.querySelector(".score-board");
-const result = document.querySelector(".result");
-const rock = document.getElementById("rock");
-const paper = document.getElementById("paper");
-const scissors = document.getElementById("scissors");
-let action_message = document.getElementById("action-message");
-let rounds = parseInt(prompt("Enter the number of rounds you would like to play..."));
-const choiceIndicator = "<sup>User</sup>";
-const AIChoiceIndicator = "<sup>AI</sup>";
+let computerScore = 0;
+let userScore_span = document.getElementById('user-score');
+let computerScore_span = document.getElementById('computer-score');
+let scoreBoard_div = document.querySelector('.score-board');
+let result_div = document.querySelector('.result');
+let rock_div = document.getElementById('rock');
+let paper_div = document.getElementById('paper');
+let scissors_div = document.getElementById('scissors');
 
-
-function AIChoice() {
+function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
 }
 
-function win(userChoice, computerChoice) {
+function convertCase(anythingIwant) {
+    if (anythingIwant === 'paper') return 'Paper';
+    if (anythingIwant === 'scissors') return 'Scissors';
+    return 'Rock';
+}
+
+function win(user, computer) {
     userScore++;
     userScore_span.innerHTML = userScore;
-    aiScore_span.innerHTML = AIScore;
-    result.innerHTML = `${choiceIndicator} ${userChoice} beats ${AIChoiceIndicator} ${computerChoice} You win!!`;
+    const userName = ' (user)'.fontsize(3).sup();
+    const compName = ' (comp)'.fontsize(3).sup();
+    result_div.innerHTML = `<p>${convertCase(user)}${userName} beats ${convertCase(computer)}${compName}. You win!</p>`;
+    const roundStatus = document.getElementById(user);
+    roundStatus.classList.add('winningStyles');
+    setTimeout(() => roundStatus.classList.remove('winningStyles'), 300);
 }
 
-function lose(userChoice, computerChoice) {
-    AIScore++;
-    userScore_span.innerHTML = userScore;
-    aiScore_span.innerHTML = AIScore;
-    result.innerHTML = `AI won: ${AIChoiceIndicator} ${computerChoice} beats ${choiceIndicator} ${userChoice} You loose!!`;
+function lose(user, computer) {
+    computerScore++;
+    computerScore_span.innerHTML = computerScore;
+    const userName = ' (user)'.fontsize(3).sup();
+    const compName = ' (comp)'.fontsize(3).sup();
+    result_div.innerHTML = `<p>${convertCase(computer)}${compName} beats ${convertCase(user)}${userName}. You lose!</p>`;
+    const roundStatus = document.getElementById(user);
+    roundStatus.classList.add('losingStyles');
+    setTimeout(() => roundStatus.classList.remove('losingStyles'), 300);
 }
 
-function draw() {
-    result.innerHTML = "ITS A DRAW!";
+function draw(user, computer) {
+    const userName = ' (user)'.fontsize(3).sup();
+    const compName = ' (comp)'.fontsize(3).sup();
+    result_div.innerHTML = `<p>It was a draw! You both chose ${convertCase(user)}</p>`;
+    const roundStatus = document.getElementById(user);
+    roundStatus.classList.add('drawStyles');
+    setTimeout(() => roundStatus.classList.remove('drawStyles'), 300);
 }
 
-function game(userChoice) {
-    const computerChoice = AIChoice();
+function game(userChoice, totalRounds) {
+    const computerChoice = getComputerChoice();
 
     switch (userChoice + computerChoice) {
-        case "paperrock":
-        case "rockscissors":
-        case "scissorspaper":
+        case 'paperrock':
+        case 'rockscissors':
+        case 'scissorspaper':
             win(userChoice, computerChoice);
             break;
-        case "rockpaper":
-        case "paperscissors":
-        case "scissorsrock":
+        case 'rockpaper':
+        case 'scissorsrock':
+        case 'paperscissors':
             lose(userChoice, computerChoice);
             break;
-        case "rockrock":
-        case "paperpaper":
-        case "scissorsscissors":
+        case 'rockrock':
+        case 'scissorsscissors':
+        case 'paperpaper':
             draw(userChoice, computerChoice);
             break;
+    }
+
+    if (userScore + computerScore === totalRounds) {
+        showFinalResult();
+    }
+}
+
+function showFinalResult() {
+    if (userScore > computerScore) {
+        result_div.innerHTML += `<p>Final Result: You won ${userScore} rounds - ${computerScore} rounds. You win!</p>`;
+    } else if (userScore < computerScore) {
+        result_div.innerHTML += `<p>Final Result: You lose!  ${userScore} - ${computerScore}</p>`;
+    } else {
+        result_div.innerHTML += `<p>Final Result: It's a draw! ${userScore} rounds each.</p>`;
     }
 }
 
 function main() {
-    return new Promise(resolve => {
-        rock.addEventListener('click', function () {
-            resolve("rock");
-        });
+    // Prompt the user for the number of rounds
+    const rounds = parseInt(prompt('Enter the number of rounds you want to play:'));
 
-        paper.addEventListener('click', function () {
-            resolve("paper");
-        });
+    // Ensure the user enters a valid number of rounds
+    if (isNaN(rounds) || rounds <= 0) {
+        alert('Please enter a valid number of rounds.');
+        return;
+    }
 
-        scissors.addEventListener('click', function () {
-            resolve("scissors");
-        });
-    });
+    // Add event listeners for user choices
+    rock_div.addEventListener('click', () => game('rock', rounds));
+    paper_div.addEventListener('click', () => game('paper', rounds));
+    scissors_div.addEventListener('click', () => game('scissors', rounds));
 }
 
-async function playGame(rounds) {
-    for (let i = 0; i < rounds; i++) {
-        const userChoice = await main();
-        game(userChoice);
-    }
-    if (rounds === rounds) {
-        action_message.innerHTML = `Final score in this round:  User ${userScore} - ${AIScore} Computer`;
-    }
-}
-
-playGame(rounds);
+main();
